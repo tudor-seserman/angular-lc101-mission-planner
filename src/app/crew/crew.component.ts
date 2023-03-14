67ebfca48697180bc71844f8ee736db7ee2bcf2c
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-03-13 19:55:56 -0500

    done copying and pasting

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,9 +13,33 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    for(const crewMember of this.crew){
+      // console.log(crewMember.name)}
+    if(crewMember.name === memberName){return}}
+    
+      this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+  save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
   }
 
 }
